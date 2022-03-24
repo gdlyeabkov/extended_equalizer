b97c6ac1e3767a32fd29b9dfc86b58e1164f3343
gdlyeabkov <gdlyeabkov@gmail.com>
2022-03-24 12:30:02 +0300

    add_slider

--- App.js
+++ App.js
@@ -1,21 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Slider from '@react-native-community/slider'
+
+const Stack = createNativeStackNavigator()
+
+const testActivity = 'MainActivity'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={testActivity}>
+        <Stack.Screen
+          name="MainActivity"
+          component={MainActivity}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
+export function MainActivity() {
+  
+  const [volume, setVolume] = useState(0)
+
+  const getVolume = () => {
+    let rawVolume = volume.toFixed(0)
+    if (volume < 10) {
+      rawVolume = `0${rawVolume}`
+    }
+    return rawVolume
+  }
+
+  return (
+    <View
+      style={styles.mainActivityContainer}
+    >
+      <View
+        style={styles.mainActivityContainerEqualizer}
+      >
+        <View
+          style={styles.mainActivityContainerEqualizerInfo}
+        >
+          <Text
+            style={styles.mainActivityContainerEqualizerInfoVolumeLabel}
+          >
+            Громкость
+          </Text>
+          <Text
+            style={styles.mainActivityContainerEqualizerInfoVolumeValue}
+          >
+            {
+              getVolume()
+            }
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          transform:[{rotate: "-90deg"}]
+        }}
+      >
+        <Slider
+          style={{width: 315, height: 40}}
+          ref={(ref) => {
+            
+          }}
+          onValueChange={(value) => {
+            setVolume(value)
+          }}
+          minimumValue={0}
+          maximumValue={100}
+          minimumTrackTintColor="rgb(150, 150, 150)"
+          maximumTrackTintColor="rgb(150, 150, 150)"
+        />
+      </View>
+    </View>
+  )
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  mainActivityContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
   },
-});
+  mainActivityContainerEqualizerInfo: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  mainActivityContainerEqualizerInfoVolumeLabel: {
+    marginLeft: 15
+  },
+  mainActivityContainerEqualizerInfoVolumeValue: {
+    marginLeft: 15,
+    color: 'rgb(255, 0, 0)'
+  }
+})
